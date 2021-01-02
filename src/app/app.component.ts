e535951f71073759fedb869717dbcf4a940cb640
unknown <zilson1987@gmail.com>
2021-01-01 22:03:59 -0600

    nearly done with search method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,13 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[];
   
   constructor() {
     this.sourceList = [];
-    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';  
+    this.displayList = [];
+    
  
     window.fetch(satellitesUrl).then(function(response) {
        response.json().then(function(data) {
@@ -25,10 +28,22 @@ export class AppComponent {
             // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
             this.sourceList.push(displaySatellite); 
           }
- 
+          this.displayList = this.sourceList.slice(0);
        }.bind(this));
     }.bind(this));
  
  }
-  
+   search(searchTerm: string): void {
+      let matchingSatellites: Satellite[] = [];
+      searchTerm = searchTerm.toLowerCase();
+      for(let i=0; i < this.sourceList.length; i++) {
+         let name = this.sourceList[i].name.toLowerCase();
+         if (name.indexOf(searchTerm) >= 0) {
+         matchingSatellites.push(this.sourceList[i]);
+         }
+      }
+   // assign this.displayList to be the array of matching satellites
+   // this will cause Angular to re-make the table, but now only containing matches
+   this.displayList = matchingSatellites;
+   }
 }
