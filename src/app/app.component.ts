6c0fe0fec0523458179d8cdab08f7428abe7aff7
unknown <zilson1987@gmail.com>
2021-01-06 21:57:38 -0600

    added search for type of satellite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,24 +7,24 @@ import { Satellite } from './satellite';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'orbit-report';
+  title: string = 'orbit-report';
   sourceList: Satellite[];
   displayList: Satellite[];
   
   constructor() {
     this.sourceList = [];
-    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';  
+    let satellitesUrl: string = 'https://handlers.education.launchcode.org/static/satellites.json';  
     this.displayList = [];
     
  
     window.fetch(satellitesUrl).then(function(response) {
        response.json().then(function(data) {
  
-          let fetchedSatellites = data.satellites;
+          let fetchedSatellites: object = data.satellites;
           // TODO: loop over satellites
           for(let i in fetchedSatellites){
             // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
-            let displaySatellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+            let displaySatellite: object = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
             // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
             this.sourceList.push(displaySatellite); 
           }
@@ -38,7 +38,11 @@ export class AppComponent {
       searchTerm = searchTerm.toLowerCase();
       for(let i=0; i < this.sourceList.length; i++) {
          let name = this.sourceList[i].name.toLowerCase();
+         let type = this.sourceList[i].type.toLowerCase();
          if (name.indexOf(searchTerm) >= 0) {
+         matchingSatellites.push(this.sourceList[i]);
+         }
+         if (type.indexOf(searchTerm) >= 0) {
          matchingSatellites.push(this.sourceList[i]);
          }
       }
